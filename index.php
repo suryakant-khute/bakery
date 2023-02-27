5c004fc97cded4ed0ab293404852f3db85516b9d
suryakant khute <suryakantkhute5@gmail.com>
2023-02-27 15:16:37 +0530

    adding all files

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bakery</title>
+    <!-- link css  -->
+    <link rel="stylesheet" href="style.css">
+    <!-- link favicon  -->
+</head>
+
+
+<body>
+    <!-- header start ============================ -->
+    <section class="bg-1">
+        <div class="container">
+            <div class="navbar flex justify">
+                <ul class="flex">
+                    <h1>LOGO</h1>
+                    <li><a href="">WELCOME</a></li>
+                    <li><a href="">MENU</a></li>
+                    <li><a href="">RESERVATIONS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+                <div class="social-icon">
+                    <svg width="36" height="36" fill="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M8.432 19.8c7.245 0 11.209-6.005 11.209-11.202 0-.168 0-.338-.007-.506A8.023 8.023 0 0 0 21.6 6.049a7.99 7.99 0 0 1-2.266.622 3.961 3.961 0 0 0 1.736-2.18 7.84 7.84 0 0 1-2.505.951 3.943 3.943 0 0 0-6.715 3.593A11.19 11.19 0 0 1 3.73 4.92a3.947 3.947 0 0 0 1.222 5.259 3.989 3.989 0 0 1-1.784-.49v.054a3.946 3.946 0 0 0 3.159 3.862 3.964 3.964 0 0 1-1.775.069 3.939 3.939 0 0 0 3.68 2.733 7.907 7.907 0 0 1-4.896 1.688 7.585 7.585 0 0 1-.936-.054A11.213 11.213 0 0 0 8.432 19.8Z">
+                        </path>
+                    </svg>
+                    <svg width="36" height="36" fill="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M22.08 12.539c0-5.335-4.298-9.66-9.6-9.66-5.304.001-9.602 4.325-9.602 9.661 0 4.82 3.511 8.817 8.1 9.541v-6.75H8.542v-2.79h2.438v-2.13c0-2.421 1.434-3.758 3.627-3.758 1.05 0 2.149.188 2.149.188v2.376h-1.21c-1.192 0-1.564.745-1.564 1.51v1.812h2.661l-.425 2.791H13.98v6.75c4.59-.725 8.1-4.72 8.1-9.541Z">
+                        </path>
+                    </svg>
+                    <svg width="36" height="36" fill="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M12 2c-2.714 0-3.055.013-4.121.06-1.066.05-1.793.217-2.429.465a4.896 4.896 0 0 0-1.771 1.154A4.909 4.909 0 0 0 2.525 5.45c-.248.635-.416 1.362-.465 2.425C2.013 8.944 2 9.284 2 12.001c0 2.715.013 3.055.06 4.121.05 1.066.217 1.792.465 2.428a4.91 4.91 0 0 0 1.154 1.771 4.88 4.88 0 0 0 1.77 1.154c.637.247 1.362.416 2.427.465 1.068.047 1.408.06 4.124.06 2.716 0 3.055-.012 4.122-.06 1.064-.05 1.793-.218 2.43-.465a4.893 4.893 0 0 0 1.77-1.154 4.91 4.91 0 0 0 1.153-1.771c.246-.636.415-1.363.465-2.428.047-1.066.06-1.406.06-4.122s-.012-3.056-.06-4.124c-.05-1.064-.219-1.791-.465-2.426a4.907 4.907 0 0 0-1.154-1.771 4.888 4.888 0 0 0-1.771-1.154c-.637-.248-1.365-.416-2.429-.465-1.067-.047-1.406-.06-4.123-.06H12Zm-.896 1.803H12c2.67 0 2.987.008 4.04.057.975.044 1.505.208 1.858.344.466.181.8.399 1.15.748.35.35.566.683.747 1.15.138.352.3.882.344 1.857.049 1.053.059 1.37.059 4.039 0 2.668-.01 2.986-.059 4.04-.044.974-.207 1.503-.344 1.856a3.09 3.09 0 0 1-.748 1.149 3.09 3.09 0 0 1-1.15.747c-.35.137-.88.3-1.857.345-1.053.047-1.37.059-4.04.059s-2.987-.011-4.041-.059c-.975-.045-1.504-.208-1.856-.345a3.097 3.097 0 0 1-1.15-.747 3.1 3.1 0 0 1-.75-1.15c-.136-.352-.3-.882-.344-1.857-.047-1.054-.057-1.37-.057-4.041 0-2.67.01-2.985.057-4.039.045-.975.208-1.505.345-1.857.181-.466.399-.8.749-1.15a3.09 3.09 0 0 1 1.15-.748c.352-.137.881-.3 1.856-.345.923-.042 1.28-.055 3.144-.056v.003Zm6.235 1.66a1.2 1.2 0 1 0 0 2.4 1.2 1.2 0 0 0 0-2.4ZM12 6.865a5.136 5.136 0 1 0-.16 10.272A5.136 5.136 0 0 0 12 6.865Zm0 1.801a3.334 3.334 0 1 1 0 6.668 3.334 3.334 0 0 1 0-6.668Z">
+                        </path>
+                    </svg>
+
+                </div>
+            </div>
+            <div class="main-heading text-center">
+                <h1>Pastry with love</h1>
+                <p>We're bringing you fresh ingredients every <br> day in way you can't resist </p>
+                <button class="btn">OUR MENU</button>
+            </div>
+        </div>
+    </section>
+    <!-- end ====================== -->
+    <!-- art of cakes start ============================== -->
+    <section class="bg-2">
+        <div class="container text-center flex justify-around">
+            <div class="left">
+                <h1>Art of cakes</h1>
+                <p id="left-p1">Lorem ipsum dolor sit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br> Nam fuga neque voluptate tempore
+                    quisquam mollitia <br> laboriosam. Dolores quae delectus laboriosam.</p>
+                <h2>chef cook</h2>
+                <img src="images/art-img.png" alt="art">
+                <p>"Quote Lorem ipsum dolor sit."</p>
+            </div>
+            <div class="right">
+                <div>
+                    <img src="images/img1.png" alt="">
+                    <img src="images/img2.png" alt="">
+                </div>
+                <div>
+                    <img src="images/img3.png" alt="">
+                    <img src="images/img4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end ================ -->
+    <!-- tasty section start =================== -->
+
+    <section id="bg-3">
+        <div class="container text-center flex justify">
+            <div class="left-img">
+                <img src="images/img-place.png" alt="">
+            </div>
+            <div class="right-text">
+                <h1>Tasty pancakes</h1>
+                <p>Lorem ipsum dolor sit.</p>
+                <p id="tasty-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolor nemo beatae
+                    consequuntur tempora neque culpa, quis et repellendus similique.</p>
+                <div class="testy-icons flex">
+                    <img src="images/img-place1.png" alt="">
+                    <img src="images/img-place2.png" alt="">
+                    <img src="images/img-place3.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end ==================== -->
+    <!-- breakfast section start =================== -->
+    <section class="bg-4">
+        <div class="container text-center flex justify-around">
+            <div class="left-text-breakfast">
+                <h1>Breakfast</h1>
+                <p>Lorem ipsum dolor sit.</p>
+                <h2>Full breakfast</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur</p>
+                <span>$49</span>
+                <h2>Full breakfast</h2>
+                <p>Lorem ipsum dolor, sit etur</p>
+                <span>$4</span>
+                <h2>Full breakfast</h2>
+                <p>Lorem ipsum dolor, etur</p>
+                <span>$3.23</span>
+            </div>
+            <div class="right-img-breakfast">
+                <div class="top-text-breakfast flex justify-around">
+                    <p>EST.</p>
+                    <p>1993</p>
+                </div>
+                <h1>Your morning <br> Escape with Flair </h1>
+                <p>Lorem ipsum dolor sit, amet consectetur <br> adipisicing elit. Eos id minima aspernatur <br> nihil
+                    quia molestiae.</p>
+                <button class="btn">READ</button>
+            </div>
+        </div>
+    </section>
+    <!-- end ==================== -->
+    <!-- sandwich section start ====================== -->
+    <section id="bg-5">
+        <div class="container">
+            <div class="sandwich-text text-center">
+                <h1>The Jibarito sandwich</h1>
+                <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="mob-flex flex justify-around">
+                <div class="left-item-sandwich text-center">
+                    <img src="images/third-placeholder2.png" alt="">
+                    <h2>Class aptent</h2>
+                    <p>Lorem ipsum, dolor sit amet <br> consectetur adipisicing elit.</p>
+                    <img src="images/third-placeholder3.png" alt="">
+                    <h2>Class aptent</h2>
+                    <p>Lorem ipsum, dolor sit amet <br> consectetur adipisicing elit.</p>
+                    <img src="images/third-placeholder4.png" alt="">
+                    <h2>Class aptent</h2>
+                    <p>Lorem ipsum, dolor sit amet <br> consectetur adipisicing elit.</p>
+                </div>
+                <div class="right-img-sandwich flex justify">
+                    <div class="recipe-btn">
+                        <button class="btn-2">FULL RECIPE</button>
+                    </div>
+                    <div class="sandwich-container-item text-center">
+                        <div class="sandwich-item">
+                            <h2>PREP</h2>
+                            <h1>15</h1>
+                            <p>mins</p>
+                        </div>
+                        <div class="sandwich-item">
+                            <h2>COOK</h2>
+                            <h1>35</h1>
+                            <p>mins</p>
+                        </div>
+                        <div class="sandwich-item">
+                            <h2>READY IN</h2>
+                            <h1>50</h1>
+                            <p>mins</p>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </section>
+    <!-- end ======================== -->
+    <!-- footer  -->
+    <footer>
+        <div class="container ">
+            <ul class="flex">
+                <li><a href="">OUR MENU</a></li>
+                <li>-</li>
+                <li><a href="">GALLERY</a></li>
+                <li>-</li>
+                <li><a href="">CULTURE</a></li>
+            </ul>
+        </div>
+    </footer>
+    <!-- link js file ======================= -->
+    <script src="script.js"></script>
+
+</body>
+
+</html>
